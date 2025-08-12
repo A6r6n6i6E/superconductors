2edab44f60213448002833d4f907073a2b8fe477
A6r6n6i6E <114140838+A6r6n6i6E@users.noreply.github.com>
2025-08-12 12:48:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,27 +8,34 @@ import itertools
 
 # --- Stałe ---
 ELECTRONEGATIVITY = {
-    "H": 2.20, "Li": 0.98, "Be": 1.57, "B": 2.04, "C": 2.55, "N": 3.04,
-    "O": 3.44, "F": 3.98, "Na": 0.93, "Mg": 1.31, "Al": 1.61, "Si": 1.90,
-    "P": 2.19, "S": 2.58, "Cl": 3.16, "K": 0.82, "Ca": 1.00, "Ti": 1.54,
-    "V": 1.63, "Cr": 1.66, "Mn": 1.55, "Fe": 1.83, "Co": 1.88, "Ni": 1.91,
-    "Cu": 1.90, "Zn": 1.65, "Ga": 1.81, "Ge": 2.01, "As": 2.18, "Se": 2.55,
-    "Br": 2.96, "Rb": 0.82, "Sr": 0.95, "Y": 1.22, "Zr": 1.33, "Nb": 1.60,
-    "Mo": 2.16, "Ru": 2.20, "Pd": 2.20, "Ag": 1.93, "Cd": 1.69, "In": 1.78,
-    "Sn": 1.96, "Sb": 2.05, "I": 2.66, "Cs": 0.79, "Ba": 0.89, "La": 1.10,
-    "Hf": 1.30, "Ac": 1.10
+    "H": 2.20, "He": None, "Li": 0.98, "Be": 1.57, "B": 2.04, "C": 2.55, "N": 3.04, "O": 3.44, "F": 3.98,
+    "Na": 0.93, "Mg": 1.31, "Al": 1.61, "Si": 1.90, "P": 2.19, "S": 2.58, "Cl": 3.16,
+    "K": 0.82, "Ca": 1.00, "Sc": 1.36, "Ti": 1.54, "V": 1.63, "Cr": 1.66, "Mn": 1.55, "Fe": 1.83, "Co": 1.88,
+    "Ni": 1.91, "Cu": 1.90, "Zn": 1.65, "Ga": 1.81, "Ge": 2.01, "As": 2.18, "Se": 2.55, "Br": 2.96,
+    "Rb": 0.82, "Sr": 0.95, "Y": 1.22, "Zr": 1.33, "Nb": 1.6, "Mo": 2.16, "Ru": 2.2, "Rh": 2.28,
+    "Pd": 2.20, "Ag": 1.93, "Cd": 1.69, "In": 1.78, "Sn": 1.96, "Sb": 2.05, "I": 2.66,
+    "Cs": 0.79, "Ba": 0.89, "La": 1.10, "Ce": 1.12, "Pm": 1.13, "Sm": 1.17, "Eu": 1.2,
+    "Gd": 1.2, "Tb": 1.1, "Dy": 1.22, "Ho": 1.23, "Er": 1.24, "Tm": 1.25, "Yb": 1.1, "Lu": 1.27, "Hf": 1.3,
+    "Ta": 1.5, "W": 2.36, "Re": 1.9, "Os": 2.2, "Ir": 2.2, "Pt": 2.28, "Au": 2.54, "Hg": 2.00, "Tl": 1.62,
+    "Pb": 2.33, "Bi": 2.02, "Po": 2.0, "At": 2.2, "Fr": 0.7, "Ra": 0.9, "Ac": 1.1, "Th": 1.3,
+    "Pa": 1.5, "U": 1.38, "Np": 1.36, "Pu": 1.28, "Am": 1.13, "Cm": 1.28, "Bk": 1.3, "Cf": 1.3, "Es": 1.3,
+    "Fm": 1.3, "Md": 1.3, "No": 1.3, "Lr": 1.3
 }
 
 ATOMIC_MASSES = {
-    "H": 1.008, "Li": 6.94, "Be": 9.01, "B": 10.81, "C": 12.01, "N": 14.01,
-    "O": 16.00, "F": 18.998, "Na": 22.99, "Mg": 24.31, "Al": 26.98, "Si": 28.09,
-    "P": 30.97, "S": 32.07, "Cl": 35.45, "K": 39.10, "Ca": 40.08, "Ti": 47.87,
-    "V": 50.94, "Cr": 52.00, "Mn": 54.94, "Fe": 55.85, "Co": 58.93, "Ni": 58.69,
-    "Cu": 63.55, "Zn": 65.38, "Ga": 69.72, "Ge": 72.63, "As": 74.92, "Se": 78.96,
-    "Br": 79.90, "Rb": 85.47, "Sr": 87.62, "Y": 88.91, "Zr": 91.22, "Nb": 92.91,
-    "Mo": 95.95, "Ru": 101.07, "Pd": 106.42, "Ag": 107.87, "Cd": 112.41, "In": 114.82,
-    "Sn": 118.71, "Sb": 121.76, "I": 126.90, "Cs": 132.91, "Ba": 137.33, "La": 138.91,
-    "Hf": 178.49, "Ac": 227.03
+    "H": 1.008, "He": 4.0026, "Li": 6.94, "Be": 9.0122, "B": 10.81, "C": 12.011, "N": 14.007, "O": 16.00, "F": 18.998,
+    "Na": 22.990, "Mg": 24.305, "Al": 26.982, "Si": 28.085, "P": 30.974, "S": 32.06, "Cl": 35.45,
+    "K": 39.098, "Ca": 40.078, "Sc": 44.956, "Ti": 47.867, "V": 50.942, "Cr": 51.996, "Mn": 54.938,
+    "Fe": 55.845, "Co": 58.933, "Ni": 58.693, "Cu": 63.546, "Zn": 65.38, "Ga": 69.723, "Ge": 72.630, "As": 74.922,
+    "Se": 78.971, "Br": 79.904, "Rb": 85.468, "Sr": 87.62, "Y": 88.906, "Zr": 91.224, "Nb": 92.906,
+    "Mo": 95.95, "Tc": 98, "Ru": 101.07, "Rh": 102.91, "Pd": 106.42, "Ag": 107.87, "Cd": 112.41, "In": 114.82,
+    "Sn": 118.71, "Sb": 121.76, "Te": 127.60, "I": 126.90, "Cs": 132.91, "Ba": 137.33, "La": 138.91,
+    "Ce": 140.12, "Pr": 140.91, "Nd": 144.24, "Pm": 145, "Sm": 150.36, "Eu": 151.96, "Gd": 157.25, "Tb": 158.93,
+    "Dy": 162.50, "Ho": 164.93, "Er": 167.26, "Tm": 168.93, "Yb": 173.05, "Lu": 174.97, "Hf": 178.49, "Ta": 180.95,
+    "W": 183.84, "Re": 186.21, "Os": 190.23, "Ir": 192.22, "Pt": 195.08, "Au": 196.97, "Hg": 200.59, "Tl": 204.38,
+    "Pb": 207.2, "Bi": 208.98, "Po": 209, "At": 210, "Fr": 223, "Ra": 226, "Ac": 227, "Th": 232.04,
+    "Pa": 231.04, "U": 238.03, "Np": 237, "Pu": 244, "Am": 243, "Cm": 247, "Bk": 247, "Cf": 251, "Es": 252,
+    "Fm": 257, "Md": 258, "No": 259, "Lr": 266
 }
 
 # --- Funkcje pomocnicze ---
@@ -186,17 +193,17 @@ st.pyplot(fig)
 # --- Sekcja: Szukanie w pełnej przestrzeni kombinacji ---
 
 st.markdown("---")
-st.header("🔍 Search in All Possible Element Combinations")
+st.header("🔍 Search Promising Hydrides in All Possible Element Combinations")
 
 col1, col2 = st.columns(2)
 with col1:
-    hf_min = st.number_input("Hf min", min_value=0.0, max_value=1.0, value=0.6, step=0.01)
-    ratio_min = st.number_input("Mx/MH min", min_value=0.0, value=0.0, step=0.1)
-    en_min = st.number_input("Electronegativity min", min_value=0.0, max_value=5.0, value=1.8, step=0.01)
+    hf_min = st.number_input("Hf_min", min_value=0.0, max_value=1.0, value=0.6, step=0.01)
+    ratio_min = st.number_input("Mx/MH_min", min_value=0.0, value=0.0, step=0.1)
+    en_min = st.number_input("Electronegativity_min", min_value=0.0, max_value=5.0, value=1.8, step=0.01)
 with col2:
-    hf_max = st.number_input("Hf max", min_value=0.0, max_value=1.0, value=1.0, step=0.01)
-    ratio_max = st.number_input("Mx/MH max", min_value=0.0, value=50.0, step=0.1)
-    en_max = st.number_input("Electronegativity max", min_value=0.0, max_value=5.0, value=2.3, step=0.01)
+    hf_max = st.number_input("Hf_max", min_value=0.0, max_value=1.0, value=1.0, step=0.01)
+    ratio_max = st.number_input("Mx/MH_max", min_value=0.0, value=50.0, step=0.1)
+    en_max = st.number_input("Electronegativity_max", min_value=0.0, max_value=5.0, value=2.3, step=0.01)
 
 elements = [el for el in ELECTRONEGATIVITY.keys() if el != "H" and ELECTRONEGATIVITY[el] is not None]
 x_values = [1, 2, 3]
